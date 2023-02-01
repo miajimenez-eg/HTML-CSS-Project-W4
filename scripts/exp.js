f5be716e32e906bd99ba4f7eacaf5b6bf98d8c6e
Mia Jimenez <miajimenez@expediagroup.com>
2023-02-01 12:49:23 +0000

    portfolio page content

---
+++ scripts/exp.js
@@ -0,0 +1,7 @@
+// function openPopUp(element) {
+//     const popUp = window.open("", "Popup", "width=300, height=200");
+//     popUp.document.write("<h1>This is the content of the popup</h1>");
+// }
+
+// const button = document.querySelector(".right-arrow");
+// button.addEventListener("click", () => openPopUp(button));
